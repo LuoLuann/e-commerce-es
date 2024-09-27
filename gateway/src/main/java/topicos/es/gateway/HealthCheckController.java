1b674536fb27fae67f4ef57ad06d4880402afb19
Magno Sillas <magno.sillas0@gmail.com>
2024-09-27 04:54:48 -0300

    corrigindo gateway e servicos

---
+++ gateway/src/main/java/topicos/es/gateway/HealthCheckController.java
@@ -0,0 +1,15 @@
+package topicos.es.gateway;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Gateway is running");
+    }
+}
+
